18fe6caaaef1aeccf61d6ca4ecd894c35b147983
Zulker Yusuf <zulkeryusuf@gmail.com>
2025-04-03 11:35:47 +0600

    wrote engine script (non_funct) for connecting with db

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,7 @@
 import requests 
 import pandas as pd
-import sqlite3
+import psycopg2
+from sqlalchemy import create_engine 
 
 url = 'https://data-api.coindesk.com/index/cc/v1/historical/days?market=cadli&instrument=BTC-USD&limit=1000&aggregate=1&fill=true&apply_mapping=true&response_format=JSON'
 
@@ -14,4 +15,7 @@ response_data = response.json()
 print(response_data)
 
 df = pd.json_normalize(response_data, 'Data')
-print(df)
+#print(df)
+
+engine = create_engine('postgresql+psycopg2://postgres:13112001@localhost/kidstime_bd')
+df.to_sql(name='Payments', con=engine, index=True, if_exists='fail')
