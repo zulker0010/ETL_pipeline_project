39f2fd7b15241a8316188923a1fca7897437fe66
Zulker Yusuf <zulkeryusuf@gmail.com>
2025-04-05 21:19:48 +0600

    fixed ingestion engine script, connected script with postgresql_db

--- api_script.py
+++ api_script.py
@@ -1,7 +1,8 @@
 import requests 
 import pandas as pd
 import psycopg2
-from sqlalchemy import create_engine 
+import sqlalchemy
+from sqlalchemy import create_engine, text
 
 url = 'https://data-api.coindesk.com/index/cc/v1/historical/days?market=cadli&instrument=BTC-USD&limit=1000&aggregate=1&fill=true&apply_mapping=true&response_format=JSON'
 
@@ -15,7 +16,16 @@ response_data = response.json()
 print(response_data)
 
 df = pd.json_normalize(response_data, 'Data')
-#print(df)
+print(df)
 
-engine = create_engine('postgresql+psycopg2://postgres:13112001@localhost/kidstime_bd')
-df.to_sql(name='Payments', con=engine, index=True, if_exists='fail')
+#test code to see if dataframe can be ported to a csv file from json
+df.to_csv('df.csv', sep=',', index=False, na_rep='Nan', header=True, compression='infer', date_format='dd/mm/yyyy', decimal='.')
+
+engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres:13112001@localhost/kidstime_bd')
+df.to_sql(name='Transactions', con=engine, index=False, if_exists='replace')
+
+#show data from Transactions table 
+with engine.connect() as connection:
+ result = connection.execute(text('SELECT * FROM public."Transactions"'))
+ for row in result:
+  print(row)
